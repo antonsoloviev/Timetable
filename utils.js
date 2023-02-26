c056c051ca764d66191d8224b9aced2c645ef85f
Anton Soloviev <led_soloviev@mail.ru>
2023-02-26 20:57:32 +0300

    chore

--- utils.js
+++ utils.js
@@ -1,17 +1,19 @@
 // Url to SYMPHOLIGHT REST API with port 5001 (for SSL) and HTTPS (SSL)
 var apiUrl = "https://127.0.0.1:5001/api/"
+// var apiUrl = "https://127.0.0.1:5001/api/"
+
 var token;
 
 async function authorizeAutomatic() {
 
-  url = apiUrl + "Auth/login";
+  const url = apiUrl + "Auth/login";
 
-  loginData = {};
+  const loginData = {};
   loginData.username = 'admin';
   loginData.password = '1234';
 
   // Call the postData function and provide the URL and the loginData object
-  response = await postData(url, loginData);
+  const response = await postData(url, loginData);
   token = response.token;
   document.getElementById("authStatus").value = "Authorized";
   document.getElementById("authStatus").classList.add("green");
